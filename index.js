b399d76f29ac67e715605adc28a15cecc48751a6
sonaisrayel <sonai@brightcom.com>
2023-08-16 08:52:43 +0300

    Add project to git

---
+++ index.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const { log } = require('console');
+const app = express();
+const port = 3000;
+app.use(bodyParser.json())
+
+const data = [{
+    name: "Marine",
+    surname: "Karapetyan"
+}]
+
+app.post('/title', (req, res) => {
+    const arr = [];
+
+    const { name, surname } = req.body
+    arr.push({
+        name, surname
+    })
+    console.log(arr);
+
+    res.send(JSON.stringify(arr, null, 2))
+    // fs.writeFileSync("file.json",JSON.stringify(data,null,2))
+})
+
+app.listen(port, () => {
+    console.log(`Server working on port ${port}`);
+})
