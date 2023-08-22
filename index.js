e2b789c425f0a43f6f7eea166211cda91f8b4ad1
sonaisrayel <sonai@brightcom.com>
2023-08-22 16:57:41 +0300

    Add classwork to git

--- index.js
+++ index.js
@@ -3,25 +3,35 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+const fs= require('fs');
+const data = require('./todo.json');
+const { v4: uuidv4 } = require('uuid');
+
+
 app.use(bodyParser.json())
 
-const data = [{
-    name: "Marine",
-    surname: "Karapetyan"
-}]
+app.post('/todos', (req, res) => {
 
-app.post('/title', (req, res) => {
-    const arr = [];
+    const { title,description,completed } = req.body
 
-    const { name, surname } = req.body
-    arr.push({
-        name, surname
-    })
-    console.log(arr);
+    const titles = data.map(d => d.title);
 
-    res.send(JSON.stringify(arr, null, 2))
-    // fs.writeFileSync("file.json",JSON.stringify(data,null,2))
+    if (!titles.includes(title)) {
+        data.push({ title,description,completed, id: uuidv4()});
+        fs.writeFileSync("todo.json",JSON.stringify(data,null,2))
+
+        return  res.send(data)
+    }
+
+    return res.status(404).send({message:'Todo is exists'})
 })
+
+
+app.post('/todos',(req,res)=> {
+    const {id} =  req.body
+
+})
+
 
 app.listen(port, () => {
     console.log(`Server working on port ${port}`);
