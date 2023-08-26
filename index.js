3e07e3dae86f7ec37baf78978b5f2c353babf5e4
Meri Badoyan <badoyanmary@gmail.com>
2023-08-26 14:56:09 +0400

    user post request created

--- index.js
+++ index.js
@@ -1,35 +1,58 @@
-
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
-const fs= require('fs');
-const data = require('./todo.json');
-const { v4: uuidv4 } = require('uuid');
+const fs = require("fs");
+const data = require("./todo.json");
+const { v4: uuidv4 } = require("uuid");
 
+app.use(bodyParser.json());
 
-app.use(bodyParser.json())
+app.post("/todos", (req, res) => {
+  const { title, description, completed } = req.body;
 
+  const titles = data.map((d) => d.title);
 
+  if (!titles.includes(title)) {
+    data.push({ title, description, completed, id: uuidv4() });
+    fs.writeFileSync("todo.json", JSON.stringify(data, null, 2));
 
-app.post('/todos', (req, res) => {
+    return res.send(data);
+  }
 
-    const { title,description,completed } = req.body
+  return res.status(404).send({ message: "Todo is exists" });
+});
 
-    const titles = data.map(d => d.title);
+app.post("/api/user", (req, res) => {
+  const { userName, email, password } = req.body;
 
-    if (!titles.includes(title)) {
-        data.push({ title,description,completed, id: uuidv4()});
-        fs.writeFileSync("todo.json",JSON.stringify(data,null,2))
+  const userNames = users.map((user) => user.userName);
+  const passwords = users.map((user) => user.password);
+  const emails = users.map((user) => user.email);
 
-        return  res.send(data)
-    }
+  if (userNames.includes(userName)) {
+    return res.status(404).send({ message: "Username exists" });
+  }
 
-    return res.status(404).send({message:'Todo is exists'})
-})
+  if (userName.length < 8 || password.length < 8) {
+    return req.status(404).send({
+      message: "Username and password must contain at least 8 symbols.",
+    });
+  }
 
+  if (emails.includes) {
+    return res.status(404).send({ message: "Email exists" });
+  }
 
+  const user = {
+    userName,
+    email,
+    password,
+  };
+
+  res.status(201).send({ message: "User is created", user: user });
+});
 
 app.listen(port, () => {
-    console.log(`Server working on port ${port}`);
-})
+  console.log(`Server working on port ${port}`);
+});
