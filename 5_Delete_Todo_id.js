3f8072491ca064d1749caf2be29b5a8961becbb5
knariksoghoyan <knariksoghoyan@gmail.com>
2023-08-27 15:27:43 +0400

    add app.delete in Index.js, change other files too

--- 5_Delete_Todo_id.js
+++ 5_Delete_Todo_id.js
@@ -3,41 +3,39 @@
 //     - Endpoint: `/todos/{todo_id}`
 //     - Response: Confirmation message
 
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 const fs = require('fs');
 const data = require('./todo.json');
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 app.use(bodyParser.json());
 
-app.delete('/todos/{todo_id}', (req, res) => {
+app.delete('/todos', (req, res) => {
 
-    const {todo_id} = req.body;
-    return res.send ({todo_id})
+    const { id, title, description, completed } = req.body;
 
-//     const deleteTodo = () => {
-//         data.forEach(element => {
-//             if (element.id === id) {
-//                 let found = data.find(el => el.title === title);
-//                 let index = data.indexOf(found);
-//                 data.splice(index, 1);
-//             }
-//             else {
-//                 // return res.status(404).send(`We dont have a title with name ${title} to delete`)
-//             }
-//         })
-//             return data;
-//         }
-// const newData = deleteTodo()
+    const deleteTodo = () => {
+        data.forEach(element => {
+            if (element.id === id) {
+                let found = data.find(el => el.id === id);
+                let index = data.indexOf(found);
+                data.splice(index, 1);
+            }
+            else {
+                // return res.status(404).send(`We dont have a title with id "${id}" to delete`)
+            }
+        })
+            return data;
+        }
+const newData = deleteTodo()
 
 
-//         fs.writeFileSync("todo.json", JSON.stringify(newData, null, 2));
-//         // return res.send(newData)
-//         return res.status(200).send(`Title ${title} successfully deleted`)
+        fs.writeFileSync("todo.json", JSON.stringify(newData, null, 2));
+        // return res.send(newData)
+        return res.status(200).send(`Title with id "${id}" successfully deleted`)
 
     });
 
