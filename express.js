dc711012c34f30aa69fdc72cb3fa60a297e7eac0
Lena Baghdasaryan <lenabaghdasaryan@mail.ru>
2023-08-28 01:32:32 +0400

    Made some changes, still doesn't work

--- express.js
+++ express.js
@@ -3,16 +3,17 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 const fs = require('fs');
-const data = require('./todos.json')
+const data = require('./todo.json')
 app.use(bodyParser.json());
 app.get('/.todos', (req,res) =>{
 const arr= [];
-todos.forEach(element => {
+data.forEach(element => {
     arr.push(element)
-    fs.writeFileSync(todosNew.json, JSON.strigify(arr, null, 2))
-    return res.send(arr)
-});
+})
+console.log(arr)
+    // fs.writeFileSync("todosNew.json", JSON.strigify(arr, null, 2))
+    return res.status(200).send(arr)
 })
 app.listen(port, () => {
-    console.log(arr)
+    console.log(`Server working on port ${port}`)
 })
