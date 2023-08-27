c6c02b551969cc05e68844d598074b62094c3851
sonaisrayel <sonai@brightcom.com>
2023-08-27 20:25:23 +0300

    Add linters and prettier to project

--- index.js
+++ index.js
@@ -1,64 +1,51 @@
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
-const fs= require('fs');
+const fs = require('fs');
 const data = require('./todo.json');
 const { v4: uuidv4 } = require('uuid');
 
-
-app.use(bodyParser.json())
-
-
+app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
+    const { title, description, completed } = req.body;
 
-    const { title,description,completed } = req.body
-
-    const titles = data.map(d => d.title);
+    const titles = data.map((d) => d.title);
 
     if (!titles.includes(title)) {
-        data.push({ title,description,completed, id: uuidv4()});
-        fs.writeFileSync("todo.json",JSON.stringify(data,null,2))
+        data.push({ title, description, completed, id: uuidv4() });
+        fs.writeFileSync('todo.json', JSON.stringify(data, null, 2));
 
-        return  res.send(data)
+        return res.send(data);
     }
 
-    return res.status(404).send({message:'Todo is exists'})
-})
+    return res.status(404).send({ message: 'Todo is exists' });
+});
 
 app.put('/todos/complete', (req, res) => {
+    const { id } = req.body;
 
-    const { id,completed } = req.body
-
-    
-
-    let file=fs.readFileSync("todo.json", 'utf8')
-    let file1=JSON.parse(file)
-    file1.forEach(el =>{
-        if (id == el.id){
-            el.completed=true
-              
+    const file = fs.readFileSync('todo.json', 'utf8');
+    const file1 = JSON.parse(file);
+    file1.forEach((el) => {
+        if (id === el.id) {
+            el.completed = true;
         }
-    })
-    fs.writeFileSync("todo.json",JSON.stringify(file1,null,2))
-    let file2=fs.readFileSync("todo.json", 'utf8')
-    let file3=JSON.parse(file2)
-    let result = file3.find(item => item.id === req.body.id);
-    res.send(result)
-})
-
+    });
+    fs.writeFileSync('todo.json', JSON.stringify(file1, null, 2));
+    const file2 = fs.readFileSync('todo.json', 'utf8');
+    const file3 = JSON.parse(file2);
+    const result = file3.find((item) => item.id === req.body.id);
+    res.send(result);
+});
 
 app.listen(port, () => {
     console.log(`Server working on port ${port}`);
-})
-
-
+});
 
 // Update Todo
 // Method: PUT/PATCH
 // Endpoint: /todos/complete
 // Request Body: { "id": "idOfTodo", "completed": true }
 // Response: Updated todo details
-
