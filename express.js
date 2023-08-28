68ab01e44c4ed9d558c4989c36a99d7940aee413
Lena Baghdasaryan <lenabaghdasaryan@mail.ru>
2023-08-29 00:49:24 +0400

    Get all todos working code

--- express.js
+++ express.js
@@ -5,14 +5,13 @@ const port = 3000;
 const fs = require('fs');
 const data = require('./todo.json')
 app.use(bodyParser.json());
-app.get('/.todos', (req,res) =>{
+app.get('/todos', (req,res) =>{
 const arr= [];
 data.forEach(element => {
     arr.push(element)
 })
 console.log(arr)
-    // fs.writeFileSync("todosNew.json", JSON.strigify(arr, null, 2))
-    return res.status(200).send(arr)
+        return res.status(200).send(arr)
 })
 app.listen(port, () => {
     console.log(`Server working on port ${port}`)
