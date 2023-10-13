92ec00083df73fc2bdefc4be74a4d074d76efd49
Meri Badoyan <badoyanmary@gmail.com>
2023-10-13 16:20:46 +0400

    getUsers and deleteUsers created

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,72 +1,53 @@
-// // const { getAll} = require('../helpers/mongodb');
-//
-// const getAllUsers = async (req, res) => {
-//     try {
-//         const users = await getAll('users');
-//         return res.status(200).send(users);
-//     } catch (err) {
-//         return res.status(404).send({ message: err.message });
-//     }
-// };
-//
-// const getUser = async (req, res) => {
-//     const { id } = req.params;
-//
-//     // const user = await getAll('users', id);
-//
-//
-//     // if (!getUs) {
-//     //     res.status(404).send('User Not Found');
-//     // } else {
-//     //     res.status(200).send(getUs);
-//     // }
-// };
-//
+// const { getAll} = require('../helpers/mongodb');
+
+const { read, del } = require('../helpers/mongodb');
+const { errorHandling } = require('../helpers/errorHandling');
+
+const getUsers = async (req, res) => {
+    const users = await read('users');
+
+    res.status(200).send(users);
+};
+
 // const authenticateUser = (req, res) => {
-//     const { email, password } = req.body;
-//    // const user = users.find((user) => user.email === email);
-//
-//     // if (!user) {
-//     //     return res.status(404).send({ message: 'No such email.' });
-//     // } else if (user.password !== password) {
-//     //     return res.status(404).send({ message: 'Wrong credentials', username: user.username });
-//     // }
-//
-//     //TODO write JWT logic
+// const { email, password } = req.body;
+// const user = users.find((user) => user.email === email);
+
+// if (!user) {
+//     return res.status(404).send({ message: 'No such email.' });
+// } else if (user.password !== password) {
+//     return res.status(404).send({ message: 'Wrong credentials', username: user.username });
+// }
+
+//TODO write JWT logic
 //     res.status(200).send({ message: 'You will receive some token soon' });
 // };
-//
+
 // const updateUser = (req, res) => {
-//     const userId = Number(req.params.id);
-//     const body = req.body;
-//
-//     const user = users.find((element) => element.id === userId);
-//     if (!user) {
-//         res.status(404).send('User Not Found');
-//     } else {
-//         const updatedProfile = { ...user, ...body };
-//         console.log(updatedProfile);
-//         res.status(200).json(updatedProfile);
+//     try {
+//         const { id } = req.params;
+//         const body = req.body;
+//     } catch (error) {
+//         res.status(404).send({ message: err.message });
 //     }
 // };
-//
-// const deleteUser = (req, res) => {
-//     const userId = Number(req.params.id);
-//
-//    // const usersData = users.filter((el) => el.id !== userId);
-//    //  if (!usersData) {
-//    //      res.status(404).send('User Not Found');
-//    //  } else {
-//    //      res.status(200).send('User successfully deleted');
-//    //      console.log(usersData);
-//    //  }
-//    //  return usersData;
-// };
-//
-// module.exports = {
-//     getAllUsers,
-//     getUser,
-//     authenticateUser,
-//     updateUser,
-//     deleteUser,
-// };
+
+const deleteUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const resp = await del('users', id);
+        if (!resp.deletedCount) {
+            errorHandling(`Id is not found`);
+        }
+        return res.status(200).send({ message: `User with id "${id}" successfully deleted` });
+    } catch (err) {
+        res.status(404).send({ message: err.message });
+    }
+};
+
+module.exports = {
+    getUsers,
+    // authenticateUser,
+    // updateUser,
+    deleteUser,
+};
--- routes/user-router.js
+++ routes/user-router.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { authenticateUser, getUser, updateUser, deleteUser } = require('../controllers/user-controller');
+const { authenticateUser, getUsers, deleteUser } = require('../controllers/user-controller');
 
-router.get('/:id?', getUser);
+router.get('/:id?', getUsers);
 // router.post('/', createUser);
 router.post('/login', authenticateUser);
-router.put('/:id', updateUser);
+// router.put('/:id', updateUser);
 router.delete('/:id', deleteUser);
 
 module.exports = router;
